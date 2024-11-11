0d12665530bfadd85eb51be5a48baf12be2f5f08
youtube-engineer <chatgptinjapan@gmail.com>
2024-11-11 10:16:25 +0000

    日付更新されない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-'use client'
+// "use server";
+// 非同期処理を行いたい場合の例
 
-export default function Home() {
-  console.log('client component');
-  // console.log({ 1 + 2 });
+
+export const dynamic = 'force-dynamic';
+
+export default async function Home() {
+
+  const now = new Date().toLocaleTimeString(); // 非同期処理（ただし、これは非同期で行う必要はありません）
 
   return (
     <div>
-      <h1>client component</h1>
+      <h1>server component</h1>
+      <p>Current time: {now}</p>
     </div>
   );
 }
--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -2,10 +2,6 @@
 
 export default function Home() {
     console.log('server component');
-
-
-
-
     return (
         <div>
             <h1>server component</h1>
